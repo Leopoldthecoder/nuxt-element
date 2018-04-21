c53d534121ff171cfce3e3ed6e877814a200aeb6
Leopoldthecoder <Leopoldthecuber@gmail.com>
2018-04-21 19:29:24 +0800

    init

---
+++ nuxt.config.js
@@ -0,0 +1,58 @@
+module.exports = {
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: 'nuxt-element',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: 'nuxt-element' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+  /*
+  ** Customize the progress bar color
+  */
+  loading: { color: '#3B8070' },
+  /*
+  ** Build configuration
+  */
+
+  build: {
+    babel: {
+      "plugins": [
+        [
+          "component",
+          {
+            "libraryName": "element-ui",
+            "styleLibraryName": "theme-chalk"
+          }
+        ]
+      ]
+    },
+    /*
+    ** Run ESLint on save
+    */
+    extend (config, { isDev, isClient }) {
+      if (isDev && isClient) {
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue)$/,
+          loader: 'eslint-loader',
+          exclude: /(node_modules)/
+        })
+      }
+    }
+  },
+
+  css: [
+    'element-ui/lib/theme-chalk/index.css'
+  ],
+
+  plugins: [
+    '@/plugins/element-ui'
+  ]
+}
---
+++ plugins/element-ui.js
@@ -0,0 +1,8 @@
+import Vue from 'vue'
+import { Button, Select, Option } from 'element-ui'
+
+export default () => {
+  Vue.use(Button)
+  Vue.use(Select)
+  Vue.use(Option)
+}
